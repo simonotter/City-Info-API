d0e4074aea27fc9e88ed933eb7466ec797675839
Simon Otter <simonotter@gmail.com>
2018-04-01 19:40:55 +0100

    Added optional naming convention in JSON serialiser

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
 
 namespace CityInfo.API
 {
@@ -16,6 +17,15 @@ namespace CityInfo.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+                    //.AddJsonOptions(o =>
+                    //{
+                    //    if (o.SerializerSettings.ContractResolver != null)
+                    //    {
+                    //        var castedResolver = o.SerializerSettings.ContractResolver
+                    //                              as DefaultContractResolver;
+                    //        castedResolver.NamingStrategy = null;
+                    //    }
+                    //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
