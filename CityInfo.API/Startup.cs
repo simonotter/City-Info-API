396b3b43892ea5250fa980781b70e5837ef7242b
Simon Otter <simonotter@gmail.com>
2018-04-02 09:44:09 +0100

    Added logging via Dependency Injection

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Serialization;
 
 namespace CityInfo.API
