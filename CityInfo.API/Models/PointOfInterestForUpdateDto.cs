c1ca1004e99b45c1671d0545351a40fe773fd693
Simon Otter <simonotter@gmail.com>
2018-04-02 08:23:13 +0100

    Validated PointOfInterestCreatePOST method and Added PointOfInterestUpdate PUT method.

--- CityInfo.API/Models/PointOfInterestForUpdateDto.cs
+++ CityInfo.API/Models/PointOfInterestForUpdateDto.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace CityInfo.API.Models
 {
     public class PointOfInterestForUpdateDto
     {
-        public PointOfInterestForUpdateDto()
-        {
-        }
+        [Required(ErrorMessage = "You should provide a name value.")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [MaxLength(200)]
+        public string Description { get; set; }
     }
 }
