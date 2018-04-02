91ef6276d25838948f174ee6c6a10756cd952b8b
Simon Otter <simonotter@gmail.com>
2018-04-02 12:08:40 +0100

    Add eMail notifications on DELETE PointOfInterest across Dev and Prod environvments.

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using CityInfo.API.Models;
+using CityInfo.API.Services;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -12,10 +13,13 @@ namespace CityInfo.API.Controllers
     public class PointsOfInterestController : Controller
     {
         private ILogger<PointsOfInterestController> _logger;
+        private IMailService _mailService;
 
-        public PointsOfInterestController(ILogger<PointsOfInterestController> logger)
+        public PointsOfInterestController(ILogger<PointsOfInterestController> logger,
+                                         IMailService mailService)
         {
             _logger = logger;
+            _mailService = mailService;
         }
 
 
@@ -214,6 +218,10 @@ namespace CityInfo.API.Controllers
             }
 
             city.PointsOfInterest.Remove(pointOfInterestFromStore);
+
+            _mailService.Send("Point of interest deleted.",
+                              $"Point of interest {pointOfInterestFromStore.Name} " +
+                              $"with id {pointOfInterestFromStore.Id} was deleted.");
 
             return NoContent();
         }
--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -10,11 +10,20 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Serialization;
 using NLog.Extensions.Logging;
+using CityInfo.API.Services;
+using Microsoft.Extensions.Configuration;
 
 namespace CityInfo.API
 {
     public class Startup
     {
+        public static IConfiguration Configuration { get; private set;  }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -23,15 +32,20 @@ namespace CityInfo.API
                     .AddMvcOptions(o => o.OutputFormatters.Add(
                        new XmlDataContractSerializerOutputFormatter()))
                     ;
-                    //.AddJsonOptions(o =>
-                    //{
-                    //    if (o.SerializerSettings.ContractResolver != null)
-                    //    {
-                    //        var castedResolver = o.SerializerSettings.ContractResolver
-                    //                              as DefaultContractResolver;
-                    //        castedResolver.NamingStrategy = null;
-                    //    }
-                    //});
+            //.AddJsonOptions(o =>
+            //{
+            //    if (o.SerializerSettings.ContractResolver != null)
+            //    {
+            //        var castedResolver = o.SerializerSettings.ContractResolver
+            //                              as DefaultContractResolver;
+            //        castedResolver.NamingStrategy = null;
+            //    }
+            //});
+#if DEBUG
+            services.AddTransient<IMailService, LocalMailService>();
+#else
+            services.AddTransient<IMailService, CloudMailService>();
+#endif
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
